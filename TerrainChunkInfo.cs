35768cfe8526c43a8dcd4f3b52b6767a4fa2ac03
lucas <lucasmessiaspereira322@gmail.com>
2025-03-01 14:39:12 -0300

    Add support for diagonal neighbor biomes and blending flags in TerrainChunkInfo

--- TerrainChunkInfo.cs
+++ TerrainChunkInfo.cs
@@ -1,23 +1,32 @@
 using UnityEngine;
+
 public class TerrainChunkInfo : MonoBehaviour
 {
     public BiomeDefinition biome;
     public bool hasDifferentNeighbor;
-
     public bool hasMultipleDifferentNeighbors;
     
-
-    // Biomas vizinhos para cada direção:
+    // Biomas vizinhos para direções cardinais:
     public BiomeDefinition neighborBiomeNorth;
     public BiomeDefinition neighborBiomeEast;
     public BiomeDefinition neighborBiomeSouth;
     public BiomeDefinition neighborBiomeWest;
-
     
-
-    // Flags para blend em cada direção:
+    // Flags para blend em direções cardinais:
     public bool blendNorth;
     public bool blendEast;
     public bool blendSouth;
     public bool blendWest;
+    
+    // Biomas vizinhos para direções diagonais:
+    public BiomeDefinition neighborBiomeNorthEast;
+    public BiomeDefinition neighborBiomeSouthEast;
+    public BiomeDefinition neighborBiomeSouthWest;
+    public BiomeDefinition neighborBiomeNorthWest;
+    
+    // Flags para blend em direções diagonais:
+    public bool blendNorthEast;
+    public bool blendSouthEast;
+    public bool blendSouthWest;
+    public bool blendNorthWest;
 }
